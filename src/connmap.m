e99767807de61c313cdbc867d3c85a56a8bbc08f
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-15 15:45:34 -0500

    partials

--- src/connmap.m
+++ src/connmap.m
@@ -5,10 +5,11 @@ Yfmri = spm_read_vols(Vfmri);
 osize = size(Yfmri);
 rYfmri = reshape(Yfmri,[],osize(4))';
 
+% Bivariate Pearson correlation
 R = corr(data,rYfmri);
 Z = atanh(R) * sqrt(size(data,1)-3);
 
-for r = 1:size(R,1)
+for r = 1:size(data,2)
 	
 	Rout = reshape(R(r,:),osize(1:3));
 	Vout = rmfield(Vfmri(1),'pinfo');
@@ -19,6 +20,27 @@ for r = 1:size(R,1)
 	Zout = reshape(Z(r,:),osize(1:3));
 	Vout = rmfield(Vfmri(1),'pinfo');
 	Vout.fname = fullfile(out_dir,sprintf('Z_%s_%03d.nii',tag,r));
+	Vout.dt(1) = spm_type('float32');
+	spm_write_vol(Vout,Zout);
+
+end
+
+
+% Partial correlation w.r.t. other seed ROIs
+for r = 1:size(data,2)
+	
+	pR = partialcorr(data(:,r),rYfmri,data(:,[1:r-1 r+1:end]));
+	pZ = atanh(pR) * sqrt(size(data,1)-3);
+
+	Rout = reshape(pR,osize(1:3));
+	Vout = rmfield(Vfmri(1),'pinfo');
+	Vout.fname = fullfile(out_dir,sprintf('pR_%s_%03d.nii',tag,r));
+	Vout.dt(1) = spm_type('float32');
+	spm_write_vol(Vout,Rout);
+	
+	Zout = reshape(pZ,osize(1:3));
+	Vout = rmfield(Vfmri(1),'pinfo');
+	Vout.fname = fullfile(out_dir,sprintf('pZ_%s_%03d.nii',tag,r));
 	Vout.dt(1) = spm_type('float32');
 	spm_write_vol(Vout,Zout);
 
--- src/connmat.m
+++ src/connmat.m
@@ -1,8 +1,17 @@
 function [R,Z] = connmat(data,out_dir,tag)
 
+% Bivariate Pearson correlation
 R = corr(data);
 Z = atanh(R) * sqrt(size(data,1)-3);
 
 csvwrite(fullfile(out_dir,['R_' tag '.csv']), R);
 csvwrite(fullfile(out_dir,['Z_' tag '.csv']), Z);
+
+
+% Partial correlation w.r.t. all other ROIs
+pR = partialcorr(data);
+pZ = atanh(pR) * sqrt(size(data,1)-3);
+
+csvwrite(fullfile(out_dir,['pR_' tag '.csv']), pR);
+csvwrite(fullfile(out_dir,['pZ_' tag '.csv']), pZ);
 
