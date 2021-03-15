8349d393a84443278fb6046e99ac44f1ba5f3c8c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-15 17:57:17 -0500

    makeroi

--- src/cerconn_main.m
+++ src/cerconn_main.m
@@ -35,62 +35,28 @@ gunzip(meanfmri_niigz);
 meanfmri_nii = strrep(meanfmri_niigz,'.gz','');
 
 
-% Resample cerseg ROIs to fMRI geometry
-fcerseg_nii = resample_roi(cerseg_nii,meanfmri_nii);
-
-
-% Load cerseg ROIs (we already have combined hemispheres)
-Vseg = spm_vol(fcerseg_nii);
-Yseg = spm_read_vols(Vseg);
-
-
-% Create mask of voxels with significant signal in the fMRI
-Vmeanfmri = spm_vol(meanfmri_nii);
-Ymeanfmri = spm_read_vols(Vmeanfmri);
-thresh = spm_antimode(Ymeanfmri(:));
-
-Yfmrimask = Ymeanfmri> thresh;
-
-Vout = Vmeanfmri;
-Vout.dt(1) = spm_type('uint16');
-Vout.pinfo(1:2) = [1 0];
-Vout.fname = fullfile(out_dir,'fmrimask.nii');
-spm_write_vol(Vout,Yfmrimask);
-
-
-% Apply mask to ROIs
-Yseg(Yfmrimask(:)==0) = 0;
-Yseg(isnan(Yseg(:))) = 0;
-Vout = Vmeanfmri;
-Vout.dt(1) = spm_type('uint16');
-Vout.pinfo(1:2) = [1 0];
-roi_nii = fullfile(out_dir,'rcerseg_masked.nii');
-Vout.fname = roi_nii;
-spm_write_vol(Vout,Yseg);
-
-
-% Check that all ROIs are present after masking
-if ~isequal( unique(Yseg(:)), expected_rois )
-	error('Missing an ROI after masking')
-end
+% Create ROI images in fMRI geometry
+[roi_nii,eroi_nii] = make_rois(meanfmri_nii,cerseg_nii,expected_rois,out_dir);
 
 
 % Get ROI time series. Save labels to file (labels the same for both
 % because same ROI image used)
 [data_removegm,vals] = extract_roi_timeseries(removegm_niigz,roi_nii);
 data_keepgm = extract_roi_timeseries(keepgm_niigz,roi_nii);
-csvwrite(fullfile(out_dir,'fmrimask-labels.csv'), vals)
+edata_removegm = extract_roi_timeseries(removegm_niigz,eroi_nii);
+edata_keepgm = extract_roi_timeseries(keepgm_niigz,eroi_nii);
+csvwrite(fullfile(out_dir,'roi-labels.csv'), vals)
 
 
 % Connectivity matrices
 connmat(data_removegm,out_dir,'removegm');
 connmat(data_keepgm,out_dir,'keepgm');
-
+connmat(edata_removegm,out_dir,'eremovegm');
+connmat(edata_keepgm,out_dir,'ekeepgm');
 
 % Connectivity maps
 connmap(data_removegm,removegm_niigz,out_dir,'removegm')
 connmap(data_keepgm,keepgm_niigz,out_dir,'keepgm')
-
-
-% Create eroded ROIs
+connmap(edata_removegm,removegm_niigz,out_dir,'eremovegm')
+connmap(edata_keepgm,keepgm_niigz,out_dir,'ekeepgm')
 
---
+++ src/make_rois.m
@@ -0,0 +1,79 @@
+function [roi_nii,eroi_nii] = make_rois(meanfmri_nii,cerseg_nii,expected_rois,out_dir)
+
+
+%% Create mask of voxels with significant signal in the fMRI
+Vmeanfmri = spm_vol(meanfmri_nii);
+Ymeanfmri = spm_read_vols(Vmeanfmri);
+thresh = spm_antimode(Ymeanfmri(:));
+
+Yfmrimask = Ymeanfmri> thresh;
+
+Vout = Vmeanfmri;
+Vout.dt(1) = spm_type('uint16');
+Vout.pinfo(1:2) = [1 0];
+Vout.fname = fullfile(out_dir,'fmrimask.nii');
+spm_write_vol(Vout,Yfmrimask);
+
+
+%% Resample cerseg ROIs to fMRI geometry and apply fmri mask
+fcerseg_nii = resample_roi(cerseg_nii,meanfmri_nii);
+Vseg = spm_vol(fcerseg_nii);
+Yseg = spm_read_vols(Vseg);
+
+Yseg(Yfmrimask(:)==0) = 0;
+Yseg(isnan(Yseg(:))) = 0;
+Vout = Vmeanfmri;
+Vout.dt(1) = spm_type('uint16');
+Vout.pinfo(1:2) = [1 0];
+roi_nii = fullfile(out_dir,'rcerseg_masked.nii');
+Vout.fname = roi_nii;
+spm_write_vol(Vout,Yseg);
+
+% Check that all ROIs are present after masking
+if ~isequal( unique(Yseg(:)), expected_rois )
+	error('Missing an ROI after masking (uneroded)')
+end
+
+
+%% Erode ROIs in T1 geometry and repeat
+Vroi = spm_vol(cerseg_nii);
+Yroi = spm_read_vols(Vroi);
+Yroi(isnan(Yroi(:))) = 0;
+vals = unique(Yroi(:));
+vals = vals(vals~=0);
+
+Yim = zeros(size(Yroi));
+for v = 1:numel(vals)
+	
+	m = Yroi==vals(v);
+	Yim = Yim + vals(v) * imerode(m,strel('sphere',1));
+	
+end
+
+% Write eroded ROIs to file
+Vout = Vroi;
+Vout.pinfo(1:2) = [1 0];
+Vout.dt(1) = spm_type('uint16');
+ecerseg_nii = fullfile(out_dir,'cerseg_eroded.nii');
+Vout.fname = ecerseg_nii;
+spm_write_vol(Vout,Yim);
+
+% Resample cerseg ROIs to fMRI geometry and apply fmri mask
+fecerseg_nii = resample_roi(ecerseg_nii,meanfmri_nii);
+Vseg = spm_vol(fecerseg_nii);
+Yseg = spm_read_vols(Vseg);
+
+Yseg(Yfmrimask(:)==0) = 0;
+Yseg(isnan(Yseg(:))) = 0;
+Vout = Vmeanfmri;
+Vout.dt(1) = spm_type('uint16');
+Vout.pinfo(1:2) = [1 0];
+eroi_nii = fullfile(out_dir,'rcerseg_masked_eroded.nii');
+Vout.fname = roi_nii;
+spm_write_vol(Vout,Yseg);
+
+% Check that all ROIs are present after masking
+if ~isequal( unique(Yseg(:)), expected_rois )
+	error('Missing an ROI after masking (eroded)')
+end
+
