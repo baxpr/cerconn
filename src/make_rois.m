9bbe8d2beaee4f051382231206657ab5d0bdad25
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-15 11:29:40 -0500

    init

---
+++ src/make_rois.m
@@ -0,0 +1,9 @@
+%% Inputs
+
+% From cersuit ATLASES_NATIVE
+cerseg_niigz = '../INPUTS/iw_Buckner_7Networks_u_a_c_rt1_seg1.nii.gz';
+
+% From connprep FILTERED_REMOVEGM_NATIVE, FILTERED_KEEPGM_NATIVE
+removegm_niigz = '../INPUTS/filtered_removegm_noscrub_nadfmri.nii.gz';
+keepgm_niigz = '../INPUTS/filtered_keep_noscrub_nadfmri.nii.gz';
+
