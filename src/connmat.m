771cead0262038aeec28f0e808780eb699b1f00b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-15 19:50:56 -0500

    outs

--- src/connmat.m
+++ src/connmat.m
@@ -1,5 +1,8 @@
 function [R,Z] = connmat(data,out_dir,tag)
 
+out_dir = [out_dir filesep 'CONNMAT_' upper(tag)];
+mkdir(out_dir);
+
 % Bivariate Pearson correlation
 R = corr(data);
 Z = atanh(R) * sqrt(size(data,1)-3);
@@ -14,4 +17,3 @@ pZ = atanh(pR) * sqrt(size(data,1)-3);
 
 csvwrite(fullfile(out_dir,['pR_' tag '.csv']), pR);
 csvwrite(fullfile(out_dir,['pZ_' tag '.csv']), pZ);
-
