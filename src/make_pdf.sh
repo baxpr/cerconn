e07afcb962b5cf307fba05eebeef42aebc42c05b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-24 14:29:15 -0500

    pdf

--- src/make_pdf.sh
+++ src/make_pdf.sh
@@ -9,11 +9,21 @@ cd ${OUTDIR}
 
 thedate=$(date)
 
+# ROI views on fMRI
 for slice in -85 -75 -65 -55 -45 -35 ; do
 	fsleyes render -of cor_${slice}.png \
 		--scene ortho --worldLoc 0 ${slice} -35 --displaySpace world --size 600 600 --yzoom 1000 \
 		--layout horizontal --hideCursor --hideLabels --hidex --hidez \
 		${WMEANFMRI} --overlayType volume \
+		${WCERSEG} --overlayType label --lut random --outline --outlineWidth 3
+done
+
+# ROI views on T1
+for slice in -85 -75 -65 -55 -45 -35 ; do
+	fsleyes render -of cort1_${slice}.png \
+		--scene ortho --worldLoc 0 ${slice} -35 --displaySpace world --size 600 600 --yzoom 1000 \
+		--layout horizontal --hideCursor --hideLabels --hidex --hidez \
+		${WMT1} --overlayType volume \
 		${WCERSEG} --overlayType label --lut random --outline --outlineWidth 3
 done
 
@@ -37,6 +47,11 @@ ${MAGICKDIR}/montage \
 	-tile 2x -trim -quality 100 -background black -gravity center \
 	-border 20 -bordercolor black page_cor.png
 
+${MAGICKDIR}/montage \
+	-mode concatenate cort1_*.png \
+	-tile 2x -trim -quality 100 -background black -gravity center \
+	-border 20 -bordercolor black page_cort1.png
+
 for roi in 001 002 003 004 005 006 007 ; do
 	${MAGICKDIR}/montage \
 		-mode concatenate ax_${roi}*.png \
@@ -53,6 +68,14 @@ ${MAGICKDIR}/convert -size 2600x3365 xc:white \
 	-gravity NorthWest -pointsize 48 -annotate +100+200 "${info_string}" \
 	page_cor.png
 
+${MAGICKDIR}/convert -size 2600x3365 xc:white \
+	-gravity center \( page_cort1.png -resize 2400x \) -composite \
+	-gravity North -pointsize 48 -annotate +0+100 \
+	"Cerebellar segmentation (Buckner7) over mean fMRI" \
+	-gravity SouthEast -pointsize 48 -annotate +100+100 "${thedate}" \
+	-gravity NorthWest -pointsize 48 -annotate +100+200 "${info_string}" \
+	page_cort1.png
+
 for roi in 001 002 003 004 005 006 007 ; do
 	${MAGICKDIR}/convert -size 2600x3365 xc:white \
 		-gravity center \( page_${roi}.png -resize 2000x \) -composite \
@@ -63,6 +86,6 @@ for roi in 001 002 003 004 005 006 007 ; do
 		page_${roi}.png
 done
 
-${MAGICKDIR}/convert page_cor.png page_0*.png cerconn.pdf
+${MAGICKDIR}/convert page_cor.png page_cort1.png page_0*.png cerconn.pdf
 
 rm *.png
